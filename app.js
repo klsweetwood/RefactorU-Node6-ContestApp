33ceb53ad4f95cefa5f9ce149c0ccf652d4c20c5
Kelsey Brisson <klbrisson@gmail.com>
2014-05-08 12:46:31 -0600

    Videos on voting page will not change if page is refreshed. New videos will only be loaded if user clicks on Next Videos. Part II: Voting is complete. Users can vote for the desired video, the video object will be updated, and the page refreshed to show current number of votes.

--- app.js
+++ app.js
@@ -7,7 +7,32 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(bodyParser());
 
+
+// List of all video submissions
 submissions = [];
+
+// List of the current videos displayed on the vote page
+currentVideos = [];
+
+// Adds two random videos from submissions to currentVideos if
+// currentVideos has 0 or 1 videos in it.
+function randomVideos() {
+	if (currentVideos.length < 2) {
+		currentVideos = _.sample(submissions, 2);
+	}
+}
+
+// If the video object has not had any votes, the function will
+// create a vote key on the object and set it equal to 1. If 
+// the video already has votes, votes will be incremented by 1.
+function vote(video) {
+	if (video.votes === undefined) {
+		video.votes = 1;
+		return;
+	}
+	video.votes++;
+}
+
 
 app.get('/', function(req, res) {
 	res.render('index');
@@ -28,22 +53,27 @@ app.get('/winner', function(req, res) {
 });
 
 app.get('/vote', function(req, res) {
-	if (submissions.length > 0) {
-		res.render('vote', {
-			randomVideos: _.sample(submissions, 2)
-		});		
-	}
-	else {
-		res.render('vote', {
-			submissions: submissions
-		});
-	}
-})
+	randomVideos();
+	res.render('vote', {
+		randomVideos: currentVideos,
+	});
+});
+
+app.get('/nextVideos', function(req, res) {
+	currentVideos = [];
+	res.redirect('/vote');
+});
 
 app.post('/formsubmit', function(req, res) {
 	submissions.push(req.body);
 	res.redirect('/submissions');
+});
+
+app.post('/vote/:videoIndex', function(req, res) {
+	vote(currentVideos[req.params.videoIndex]);
+	res.redirect('/vote');
 })
+
 
 var server = app.listen(8041, function() {
 	console.log('Express server listening on port ' + server.address().port);
